e2aa96ac4f8f1e8b8ba2d6384fab1314298bb348
Lee SangHyun <simon010809@gmail.com>
2025-06-25 16:31:59 +0900

    feat: db.json 생성 후 json-server로 api 호출 테스트

--- screens/category/CategoryListScreen.tsx
+++ screens/category/CategoryListScreen.tsx
@@ -14,6 +14,25 @@ const CategoryListScreen = () => {
   const route = useRoute<RouteProp<RootStackParamList, "CategoryListScreen">>();
   const { label } = route.params;
 
+  // label -> api 파라미터용 Id로 변경
+  const categoryMap: Record<string, string> = {
+    스포츠: "sports",
+    "외국/언어": "language",
+    댄스: "dance",
+    봉사활동: "volunteer",
+    자기계발: "self-dev",
+    "독서/글": "book",
+    "문화/공연": "festival",
+    "음악/악기": "music",
+    여행: "trip",
+    "업종/직무": "work",
+  };
+
+  const categoryId = categoryMap[label];
+
+  // 전체,인기,최신 상태(기본 상태는 전체)
+  // const [sort, setSort] = useState<"latest" | "popular" | "all">("all");
+
   const types = ["개인", "동아리"];
   const [selectedType, setSelectedType] = useState("개인");
   const navigation = useNavigation();
@@ -59,13 +78,13 @@ const CategoryListScreen = () => {
       {selectedType === "개인" && (
         <>
           <SortButtons />
-          <IndividualTab />
+          <IndividualTab categoryId={categoryId} />
         </>
       )}
       {selectedType === "동아리" && (
         <>
           <SortButtons />
-          <ClubTab />
+          <ClubTab categoryId={categoryId} />
         </>
       )}
     </View>
--- screens/category/tabs/ClubTab.tsx
+++ screens/category/tabs/ClubTab.tsx
@@ -1,86 +1,116 @@
 import { View, Text, StyleSheet } from "react-native";
 import { MatchingListItem } from "../../../components/MatchingList";
 import { FlatList } from "react-native-gesture-handler";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const clubData = [
-  {
-    id: "1",
-    title: "주말 풋살 모임",
-    category: "독서/글",
-    description: "수업 끝나고 7시부터 9시까지",
-    member: "45",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: "2",
-    title: "주말 풋살 모임",
-    category: "독서/글",
-    description: "수업 끝나고 7시부터 9시까지",
-    member: "45",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: "3",
-    title: "주말 풋살 모임",
-    category: "독서/글",
-    description: "수업 끝나고 7시부터 9시까지",
-    member: "45",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: "4",
-    title: "주말 풋살 모임",
-    category: "독서/글",
-    description: "수업 끝나고 7시부터 9시까지",
-    member: "45",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: "5",
-    title: "주말 풋살 모임",
-    category: "독서/글",
-    description: "수업 끝나고 7시부터 9시까지",
-    member: "45",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: "6",
-    title: "주말 풋살 모임",
-    category: "독서/글",
-    description: "수업 끝나고 7시부터 9시까지",
-    member: "45",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: "7",
-    title: "주말 풋살 모임",
-    category: "독서/글",
-    description: "수업 끝나고 7시부터 9시까지",
-    member: "45",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: "8",
-    title: "주말 풋살 모임",
-    category: "독서/글",
-    description: "수업 끝나고 7시부터 9시까지",
-    member: "45",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: "9",
-    title: "주말 풋살 모임",
-    category: "독서/글",
-    description: "수업 끝나고 7시부터 9시까지",
-    member: "45",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-];
+// 목업 데이터 (api 연결 시 삭제)
+// const clubData = [
+//   {
+//     id: "1",
+//     title: "주말 풋살 모임",
+//     category: "독서/글",
+//     description: "수업 끝나고 7시부터 9시까지",
+//     member: "45",
+//     imageUrl: "https://via.placeholder.com/150",
+//   },
+//   {
+//     id: "2",
+//     title: "주말 풋살 모임",
+//     category: "독서/글",
+//     description: "수업 끝나고 7시부터 9시까지",
+//     member: "45",
+//     imageUrl: "https://via.placeholder.com/150",
+//   },
+//   {
+//     id: "3",
+//     title: "주말 풋살 모임",
+//     category: "독서/글",
+//     description: "수업 끝나고 7시부터 9시까지",
+//     member: "45",
+//     imageUrl: "https://via.placeholder.com/150",
+//   },
+//   {
+//     id: "4",
+//     title: "주말 풋살 모임",
+//     category: "독서/글",
+//     description: "수업 끝나고 7시부터 9시까지",
+//     member: "45",
+//     imageUrl: "https://via.placeholder.com/150",
+//   },
+//   {
+//     id: "5",
+//     title: "주말 풋살 모임",
+//     category: "독서/글",
+//     description: "수업 끝나고 7시부터 9시까지",
+//     member: "45",
+//     imageUrl: "https://via.placeholder.com/150",
+//   },
+//   {
+//     id: "6",
+//     title: "주말 풋살 모임",
+//     category: "독서/글",
+//     description: "수업 끝나고 7시부터 9시까지",
+//     member: "45",
+//     imageUrl: "https://via.placeholder.com/150",
+//   },
+//   {
+//     id: "7",
+//     title: "주말 풋살 모임",
+//     category: "독서/글",
+//     description: "수업 끝나고 7시부터 9시까지",
+//     member: "45",
+//     imageUrl: "https://via.placeholder.com/150",
+//   },
+//   {
+//     id: "8",
+//     title: "주말 풋살 모임",
+//     category: "독서/글",
+//     description: "수업 끝나고 7시부터 9시까지",
+//     member: "45",
+//     imageUrl: "https://via.placeholder.com/150",
+//   },
+//   {
+//     id: "9",
+//     title: "주말 풋살 모임",
+//     category: "독서/글",
+//     description: "수업 끝나고 7시부터 9시까지",
+//     member: "45",
+//     imageUrl: "https://via.placeholder.com/150",
+//   },
+// ];
 
-const ClubTab = () => {
+const ClubTab = ({
+  categoryId,
+}: // sort,
+{
+  categoryId: string;
+  // sort: string;
+}) => {
+  // 동아리 상태 저장
+  const [clubData, setClubData] = useState<any[]>([]);
+
+  // 동아리 모임 좋아요 상태 저장
   const [likedItems, setLikedItems] = useState<string[]>([]);
 
+  // 동아리 모임 api 호출
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // json-server로 api 테스트
+        const response = await fetch(
+          `http://10.0.2.2:3001/clubs?category=${categoryId}`
+        );
+        const json = await response.json();
+        setClubData(json);
+      } catch (error) {
+        console.error("데이터 불러오기 실패: ", error);
+      }
+    };
+
+    fetchData();
+  }, [categoryId]);
+
+  // 좋아요 버튼 액션
   const handleToggleLike = (item: { id: string }) => {
     setLikedItems((prev) =>
       prev.includes(item.id)
@@ -89,6 +119,7 @@ const ClubTab = () => {
     );
   };
 
+  // 특정 모임 클릭 시 액션
   const handleItemPress = (item: any) => {
     console.log("Pressed item:", item); // 나중에 navigation 연결도 가능
   };
